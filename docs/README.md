57377729434f79cc312d574ed73eb09cb163a182
gustavobruder <gugabruder@gmail.com>
2021-08-28 17:53:43 -0300

    Ajustado inicialização do projeto e README

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,14 @@
 ﻿# Caça palavras
 Algoritmo de resolução de caça palavras em C#.
+
+# Como rodar
+TODO
+
+# Como testar
+TODO
+
+# Feature
+- [x] Buscar palavras na horizontal e vertical.
+- [ ] Buscar palavras na diagonal.
+- [ ] Matriz do caça palavras parametrizável.
+- [ ] Interface de interação com usuário.
--- src/CacaPalavras.Aplicacao/Program.cs
+++ src/CacaPalavras.Aplicacao/Program.cs
@@ -2,11 +2,38 @@
 
 namespace CacaPalavras.Aplicacao
 {
+    /*
+     * @author: Gustavo Baroni Bruder
+     */
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nCaça Palavras!\n");
+
+            var matrizLetras = new MatrizLetras();
+            matrizLetras.MostrarMapa();
+
+            var palavras = new[]
+            {
+                "VETOR",
+                "MAP",
+                "ERRO",
+                "IFELSE",
+                "CLASSE",
+                "WHILE",
+            };
+            Console.WriteLine($"\nPalavras para encontrar: {string.Join(", ",  palavras)}.\n");
+
+            var matriz = matrizLetras.Matriz;
+            var buscador = new BuscadorPalavra(matriz);
+
+            foreach (var palavra in palavras)
+            {
+                var encontrouPalavra = buscador.BuscarPalavra(palavra);
+                var mensagemPalavra = encontrouPalavra ? "foi encontrada" : "não foi encontrada";
+                Console.WriteLine($"Palavra {palavra.PadRight(8)} {mensagemPalavra}.");
+            }
         }
     }
 }
